9e001e4ab5a0df4d1b691461e7da9478caede583
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2022-02-12 12:01:38 -0600

    Add Unreal Project Files

---
+++ Source/GravityGame/GravityGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GravityGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAVITYGAME_API AGravityGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
